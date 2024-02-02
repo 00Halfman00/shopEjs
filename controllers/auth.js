a90ae991a4dd7490de0759ebe5ef52d0c462caa2
oscar sanchez <00oscarsanchez00@gmail.com>
2024-02-02 17:35:03 -0600

    hashing passwords and rendering proper responses to incorrect login or signup attempts

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,31 +1,42 @@
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     pageTitle: 'Login',
     user: req.session.user,
+    note: false,
     isAuthenticated: req.session.isAuthenticated,
   });
 };
 
 exports.postLogin = (req, res, next) => {
   const { email, password } = req.body;
-  if (
-    email &&
-    email === '00johnrambo00@somesome.com' &&
-    password &&
-    password === 'getSomeSome'
-  ) {
-    User.findOne({ email: email }).then((user) => {
-      req.session.isAuthenticated = true;
-      req.session.user = user;
-      req.session.save((err) => {
-        err ? console.log(err) : '';
-        res.redirect('/');
-      });
-    });
-  } else {
-    res.redirect('/login');
+  if (email.trim()[0] && password.trim()[0]) {
+    User.findOne({ email: email })
+      .then((user) => {
+        if (!user) {
+          const note = 'USER NOT FOUND';
+          res.render('auth/login', {
+            pageTitle: 'Login',
+            note: note,
+            isAuthenticated: req.session.isAuthenticated,
+          });
+        }
+        if(user){
+          return bcrypt
+          .compare(password, user.password)
+          .then(() => {
+            req.session.isAuthenticated = true;
+            req.session.user = user;
+            req.session.save((err) => {
+              err ? console.log(err) : '';
+              res.redirect('/');
+            });
+          })
+        }
+      })
+      .catch((err) => console.log(err));
   }
 };
 
@@ -38,41 +49,49 @@ exports.getSignup = (req, res, next) => {
   res.render('auth/login', {
     pageTitle: 'Signup',
     user: req.session.user,
+    note: false,
     isAuthenticated: req.session.isAuthenticated,
   });
 };
 
 exports.postSignup = (req, res, next) => {
   const { email, password, confirmPassword } = req.body;
-  console.log('post signup')
   if (
-    password.trim()[0] &&
-    confirmPassword.trim()[0] &&
+    password.trim() &&
+    confirmPassword.trim() &&
     password === confirmPassword &&
-    password.lenght > 7 &&
+    password[0] &&
+    password.length > 7 &&
     email.includes('@')
   ) {
-    User.findOne({ email: email })
-      .then((u) => {
-        if (u) {
-          res.redirect('/signup');
-        } else {
-          const user = new User({
-            password: password,
-            email: email,
-            cart: { items: [] },
-          });
-          return user.save();
-        }
-      })
-      .then(() => {
-        res.redirect('/login');
+    bcrypt.genSalt(12, function (err, salt) {
+      bcrypt.hash(password, salt, function (err, hash) {
+        User.findOne({ email: email })
+          .then((u) => {
+            if (u) {
+              return false;
+            } else {
+              const user = new User({
+                password: hash,
+                email: email,
+                cart: { items: [] },
+              });
+              return user.save();
+            }
+          })
+          .then((user) => {
+            if (user) res.redirect('/login');
+            else {
+              const note = 'USER WITH THAT EMAIL ALREADY EXIST';
+              res.render('auth/login', {
+                pageTitle: 'Signup',
+                note: note,
+                isAuthenticated: req.session.isAuthenticated,
+              });
+            }
+          })
+          .catch((err) => console.log(err));
       });
+    });
   }
-
-  // res.render('auth/login', {
-  //   pageTitle: 'Signup',
-  //   user: req.session.user,
-  //   isAuthenticated: req.session.isAuthenticated,
-  // });
 };
